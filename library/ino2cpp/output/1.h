9559df9b6b49eb4c3e9a85c8c3eb12957b8220b4
Imron jehleh <imron.jehleh@gmail.com>
2024-03-22 16:03:39 +0700

    20240322 T1603 CPE2422_reminder: Move ino2cpp into library/.

--- library/ino2cpp/output/1.h
+++
@@ -1,6 +0,0 @@
-void updateScale();
-void isrEncoder();
-void isrSwitch();
-void setup();
-void loop();
-void precrtajScale();
--- library/ino2cpp/output/2.h
+++
@@ -1,7 +0,0 @@
-void setup();
-void loop();
-void show_sequence();
-void get_sequence();
-void generate_sequence();
-void wrong_sequence();
-void right_sequence();
