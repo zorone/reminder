18dc884cb821e04ba361d444cae27b0a2fae24ad
Imron jehleh <imron.jehleh@gmail.com>
2024-04-13 18:28:27 +0700

    reminder: Ummm...

--- platformIO/screenTest/src/main.cpp
+++ platformIO/screenTest/src/main.cpp
@@ -1,19 +1,21 @@
-/** 
-* Blink with debugger 
-*/ 
-#include "Arduino.h" 
-#include "avr8-stub.h" 
-#include "app_api.h"   // only needed with flash breakpoints 
-void setup() 
-{   
-pinMode(LED_BUILTIN, OUTPUT); 
-// initialize the avr-debugger 
-debug_init(); 
-} 
-void loop() 
-{   
-digitalWrite(LED_BUILTIN, HIGH);   
-delay(300);   
-digitalWrite(LED_BUILTIN, LOW); 
-delay(100); 
-} 
+/**
+* Blink with debugger
+*/
+#include <Arduino.h>
+#include <avr8-stub.h>
+#include <app_api.h>   // only needed with flash breakpoints
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize the avr-debugger
+  debug_init();
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+}
