e18f8466699066d2f3e5f8eec28af07573914d3c
Imron jehleh <imron.jehleh@gmail.com>
2024-04-13 21:41:04 +0700

    reminder: Wait, why it won't work...

--- platformIO/screenTest/src/main.cpp
+++ platformIO/screenTest/src/main.cpp
@@ -5,6 +5,11 @@
 #include <avr8-stub.h>
 #include <app_api.h>   // only needed with flash breakpoints
 
+// run:
+// .\library\avr_debug\hub4com-2.1.0.0-386\com2tcp --baud 115200 \\.\COM9 11000 
+// at the root directory of the project.
+// Before debugging.
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
